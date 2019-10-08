3ee78a2cb41e8cdc320dc2307a83bb28a95fc295
glmack <glmack@gmail.com>
2019-10-08 16:08:40 -0700

    Add def represent_references func

--- multilingual_machines.py
+++ multilingual_machines.py
@@ -9,3 +9,8 @@ def clean_punctuation(texts):
     import string
     cleaned_tokens = [[word.strip(string.punctuation) for word in sentence] for sentence in texts]
     return cleaned_tokens
+
+def represent_references(reference1, reference2, candidate_sentences):
+    """Represent references to pass to corpus_bleu"""
+    refs = [reference1[:] + reference2[:]] * candidate_sentences
+    return refs
